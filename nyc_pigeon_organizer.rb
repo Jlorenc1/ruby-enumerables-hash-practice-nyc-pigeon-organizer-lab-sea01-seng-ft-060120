a380ea3d0ee03e48947176f6d89ee9e0bf9f0442
Joshua Lorenc <joshua.lorenc@icloud.com>
2020-05-20 22:13:42 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,7 +4,8 @@ def nyc_pigeon_organizer(data)
   data.each_with_object({}) do | (key, value), final_array|
   
     value.each do |inner_key, names|
-    
+      
+      names.each do
     
     end
         
